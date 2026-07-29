898f00d3261ef9960c5833b87ab1a5432c5fb722
Jansen Barros <edisiojansen@gmail.com>
2026-07-29 20:05:39 -0300

    Estrutura inicial e script do Windows

---
+++ scripts/win_audit.ps1
@@ -0,0 +1,10 @@
+# 1. Cria a pasta principal do projeto
+mkdir SecAudit-Toolkit
+
+# 2. Entra na pasta que você acabou de criar
+cd SecAudit-Toolkit
+
+# 3. Cria as subpastas para organizar os arquivos
+mkdir scripts
+mkdir dashboard
+mkdir docs
